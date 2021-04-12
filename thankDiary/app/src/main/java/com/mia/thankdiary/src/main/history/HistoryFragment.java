398f70c15a606e9994a7d8a41588fa05a08b16de
mia <suy0226@naver.com>
2021-04-12 23:29:40 +0900

    style: 달력 추가

--- thankDiary/app/src/main/java/com/mia/thankdiary/src/main/history/HistoryFragment.java
+++ thankDiary/app/src/main/java/com/mia/thankdiary/src/main/history/HistoryFragment.java
@@ -2,15 +2,20 @@ package com.mia.thankdiary.src.main.history;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import com.mia.thankdiary.R;
 import com.mia.thankdiary.databinding.FragmentHistoryBinding;
 import com.mia.thankdiary.src.common.BaseFragment;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 public class HistoryFragment extends BaseFragment<FragmentHistoryBinding> {
 
@@ -20,7 +25,26 @@ public class HistoryFragment extends BaseFragment<FragmentHistoryBinding> {
 
         binding = FragmentHistoryBinding.inflate(inflater);
 
+        initListener();
         return binding.getRoot();
+    }
+
+    private void initListener() {
+//        binding.historyCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+//            @Override
+//            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+//
+//            }
+//        });
+//        binding.historyCalendar.selec
+        binding.historyCalendar.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                showLog("History", date.toString());
+            }
+        });
+
+//        binding.historyCalendar.addDecorator(new EventDe);
     }
 
 
